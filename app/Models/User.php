42b0454edf42f1c89296c06ded5f5e9a7a8296ac
Koen <k.brouwer07@student.alfa-college.nl>
2025-10-28 09:47:00 +0100

    added contacts table

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function contacts()
+    {
+        return $this->belongsToMany(User::class, 'contacts', "user  _id", "contact_id");
+    }
 }
--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -31,7 +31,8 @@
         <article class="bg-gray-50 dark:bg-gray-700 p-4 rounded-lg shadow hover:shadow-lg transition">
           <div class="flex items-center mb-3">
             <img src="{{ asset('img/Napper-logoV2.png') }}" alt="profile picture" class="w-10 h-10 rounded-full">
-            <h1 class="ml-3 text-lg font-semibold text-gray-800 dark:text-white"><?= $post->title ?></h1>
+            <h2 class="text-xl text-white font-semibold"><?= $post->user->name ?></h2>
+            <h1 class="ml-3 text-lg ml-auto font-semibold text-gray-800 dark:text-white"><?= $post->title ?></h1>
           </div>
           <p class="text-gray-700 dark:text-gray-300"><?= $post->content ?></p>
           <div class="mt-4 flex justify-end space-x-3">
@@ -41,9 +42,6 @@
         </article>
         @endforeach
         <!-- Post card end -->
-        @if ($posts->isEmpty())
-        <p class="text-white text-xl text-center text-gray-500">No posts yet 😢</p>
-        @endif
       </main>
 
 
